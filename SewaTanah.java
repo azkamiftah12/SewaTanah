60379a0b2e82ff685bac8278be89db0923c89494
Azka Miftah Muhammad <azkamiftah29@gmail.com>
2022-01-05 19:54:06 +0700

    menambahkan harga sewa

---
+++ SewaTanah.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sewatanah;
+
+/**
+ *
+ * @author Azka Miftah
+ */
+public class SewaTanah {
+    public double sewa (double luas, double trfpermtr){
+        double HargaSewa;
+        HargaSewa = luas * trfpermtr;
+        return HargaSewa;
+    }
+}
